124066581afd099c5dadf2fdabbfe981b151d54f
Mark Kelly-Smith <dev@mark.kellysmith.uk>
2020-09-25 17:18:07 +0100

    new cask install file

---
+++ brew-casks.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+#
+# CASKS
+#
+
+# Java
+#brew cask install caskroom/versions/java7
+# Default use java8
+brew cask install java
+
+brew cask install 1password
+
+brew cask install iterm2
+
+brew cask install cyberduck
+
+brew cask install dropbox
+brew cask install google-drive
+
+brew cask install skype
+brew cask install zoom
+brew cask install slack
+
+brew cask install alfred
+brew cask alfred link
+
+brew cask install onyx
+
+# for compare file
+brew cask diffmerge
+
+# VM
+brew cask install virtualbox
+brew cask install vagrant
+vagrant plugin install vagrant-vbguest
+brew cask install docker
+
+# DB Monitor
+#brew cask install sequel-pro
+#brew cask install pgadmin3
+#brew cask install dbeaver-enterprise
+
+#Redis Monitor
+#brew cask install rdm
+
+# browser
+#brew cask install google-chrome
+#brew cask install google-chrome-canary
+#brew cask install firefox
+#brew cask install firefox-beta
+#brew cask install safari-technology-preview
+
+# Personal Kanban
+#brew cask install teamviz
+#brew cask install tomighty
+
+
+
+
+
+
+
+
